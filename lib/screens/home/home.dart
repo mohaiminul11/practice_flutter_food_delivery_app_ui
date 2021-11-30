a9960ee5a25c38ee8e3ce87e1af01452748203e1
mohaiminul11 <mi.sumon93@gmail.com>
2021-11-30 11:42:11 +0600

    initial commit

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_app_ui/constants/colors.dart';
+import 'package:flutter_food_delivery_app_ui/widgets/custom_app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: kBackground,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(Icons.arrow_back_ios_outlined,Icons.search)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final IconData leftIcon;
+  final IconData rightIcon;
+  final Function? leftCallback;
+
+  CustomAppBar(this.leftIcon, this.rightIcon, [this.leftCallback]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: leftCallback != null ? () => leftCallback!() : null,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Icon(leftIcon),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: Icon(rightIcon),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
